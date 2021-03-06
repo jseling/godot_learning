c1ce144a3d028b1787c847de2f475244f2f10e63
jerson.seling <jerson.seling@softplan.com.br>
2021-03-06 16:44:50 -0300

    first tests

---
+++ Basic_Tests_And_Helloworlds/test_1_helloworld_text/Label.gd
@@ -0,0 +1,6 @@
+extends Label
+
+
+func _ready():
+	text = "Olá mundo!"
+	pass
---
+++ Basic_Tests_And_Helloworlds/test_2_kinematicbody2d/KinematicBody2D.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var direcao = Vector2()
+
+func _process(delta):
+	direcao = Vector2()
+	
+	if Input.is_action_pressed("ui_left"):
+		direcao.x = -1
+
+	if Input.is_action_pressed("ui_right"):
+		direcao.x = 1
+
+	if Input.is_action_pressed("ui_up"):
+		direcao.y = -1
+
+	if Input.is_action_pressed("ui_down"):
+		direcao.y = 1
+
+func _physics_process(delta):
+	move_and_slide(direcao * 666)
