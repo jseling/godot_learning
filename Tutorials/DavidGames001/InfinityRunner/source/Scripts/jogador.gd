e3b03f7e6f2abbf45bd8663eb4a7432e56a1a8d0
jerson.seling <jerson.seling@softplan.com.br>
2021-03-23 19:37:48 -0300

    extra pulo duplo

--- Tutorials/DavidGames001/InfinityRunner/source/Scripts/jogador.gd
+++ Tutorials/DavidGames001/InfinityRunner/source/Scripts/jogador.gd
@@ -4,9 +4,11 @@ var gravidade = 1000
 var pulo = -800
 var limite = 600
 var velocidade = Vector2()
-var no_chao = false
+#var no_chao = false
 var slow = false
 var f_grav = 3
+var n_pulos_max = 2
+var n_pulos_atual = 0
 
 func _ready():
 	pass # Replace with function body.
@@ -19,14 +21,22 @@ func _physics_process(delta):
 		velocidade.y += gravidade * delta * f_grav
 		slow = false
 	
-	if Input.is_action_just_pressed("pulo") and no_chao:
+#	if Input.is_action_just_pressed("pulo") and no_chao:
+#		velocidade.y = pulo
+#		no_chao = false
+#		$sprite.play("pulando")
+
+	if Input.is_action_just_pressed("pulo") and n_pulos_atual < n_pulos_max:
 		velocidade.y = pulo
-		no_chao = false
+#		no_chao = false
+		n_pulos_atual += 1
 		$sprite.play("pulando")
+
 	position += velocidade * delta
 	if position.y >= limite:
 		position.y = limite
-		no_chao = true
+#		no_chao = true
+		n_pulos_atual = 0
 		slow = true
 		$sprite.play("correndo")
 #		get_node("sprite").play("correndo")
