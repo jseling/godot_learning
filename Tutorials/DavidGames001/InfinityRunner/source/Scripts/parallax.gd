420aa3383777cd054e2497ca1bff8aeebf66d49f
jerson.seling <jerson.seling@softplan.com.br>
2021-03-27 20:19:05 -0300

    ep7 parallax

---
+++ Tutorials/DavidGames001/InfinityRunner/source/Scripts/parallax.gd
@@ -0,0 +1,13 @@
+extends ParallaxBackground
+
+
+var vel = 600
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	scroll_offset.x -= vel * delta
