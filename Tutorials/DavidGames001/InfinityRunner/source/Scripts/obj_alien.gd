899af4db9ba6844afadb806a048baadc7ca375d3
jerson.seling <jerson.seling@softplan.com.br>
2021-03-27 19:37:36 -0300

    ep6 obstáculo

---
+++ Tutorials/DavidGames001/InfinityRunner/source/Scripts/obj_alien.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var vel = 600
+var velocidade = Vector2()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("criado")
+	velocidade = Vector2(-1, 0)
+	$sprite.play("default")
+
+
+func _physics_process(delta):
+	position.x += velocidade.x * delta * vel
+
+
+func _on_vis_screen_exited():
+	print("destruido")
+	queue_free()
---
+++ Tutorials/DavidGames001/InfinityRunner/source/Scripts/obj_medio.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var vel = 600
+var velocidade = Vector2()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("criado")
+	velocidade = Vector2(-1, 0)
+
+
+func _physics_process(delta):
+	position.x += velocidade.x * delta * vel
+
+
+func _on_vis_screen_exited():
+	print("destruido")
+	queue_free()
